97ced9b0da174f3eb33be3a888a64fb08b6aaaf7
Makenna <mewilk561@gmail.com>
2024-03-09 22:35:02 -0700

    agency.jsrestricted upgrades with switch statement

--- startup/agency.js
+++ startup/agency.js
@@ -1,18 +1,75 @@
 class Agency{
+    //TODO: make property collections of each type of upgrade
+
     constructor(location){
         Object.defineProperty(this, "location", {
             value: location,
             writable: false,
             configurable: false,
-          });
+        });
+
+        this.travel = [];
+        this.attractions = [];
+        this.hospitality = [];
     }
 }
 
-class Travel extends Upgrade{}
+class Travel extends Upgrade{
+    constructor(type, price, imgpath){
+        switch (type) {
+            case "Train":
+                break;
+            case "Bus":
+                break;
+            case "Cruise Ship":
+                break;
+            case "Plane":
+                break;
+            case "Jet":
+                break;
+            default:
+                throw "That's not a vehicle you can buy!";
+        }
+        super(type, price, imgpath);
+    }
+}
 
-class Attraction extends Upgrade{}
+class Attraction extends Upgrade{
+    constructor(type, price, imgpath){
+        switch (type) {
+            case "Tour Guide":
+                break;
+            case "Dining Experience":
+                break;
+            //case location spectial
+            default:
+                throw "That attraction doesn't exist!";
+        }
+        super(type, price, imgpath);
+    }
+}
 
-class Hospitality extends Upgrade{}
+class Hospitality extends Upgrade{
+    constructor(type, price, imgpath){
+        switch (type) {
+            case "Hostel":
+                break;
+            case "Bead and Breakfast":
+                break;
+            case "Inn":
+                break;
+            case "Motel":
+                break;
+            case "Hotel":
+                break;
+            case "Resort":
+                break;
+            default:
+                throw "That's not a vehicle you can buy!";
+        }
+        super(type, price, imgpath);
+    }
+}
 
 class Upgrade{
     #name;
@@ -40,10 +97,20 @@ class Upgrade{
 
 class Location{
     #name;
-    #spattraction;
-    constructor(name, spattraction){
-        this.#name=name;
-        this.#spattraction = spattraction;
+    #attraction;
+    #price
+
+    constructor(name){
+        switch (name){
+            case "Grand Canyon":
+                this.#attraction = "Colorado Rapids";
+                this.#price = 0;
+                break;
+            //TODO add more locations
+            default:
+                throw "That's not a place you can expand!"
+            }
+        this.#name = name;
     }
 
     name(){
@@ -51,6 +118,10 @@ class Location{
     }
 
     attraction(){
-        return this.#spattraction;
+        return this.#attraction;
+    }
+
+    price(){
+        return this.#price;
     }
 }
