a5d08c85a044b869d858a673395cdf7407c5c39f
Makenna <mewilk561@gmail.com>
2024-03-13 04:35:09 -0600

    saved some csvs as js strings. delete with server

---
+++ startup/csv.js
@@ -0,0 +1,103 @@
+export const GrandCanyon = 
+`Upgrade,Travel,Hospitality,Attraction
+Bus,0,0,0
+Hostel,1,0,0
+Train,1,0,0
+B&B,1,1,0
+Inn,1,2,0
+New Location,2,3,0
+Dining Experience,2,3,0
+Motel,2,3,1
+Hotel,2,4,1
+Tour Guide,2,3,1
+Plane,2,4,1
+Resort,2,5,1
+Jet,3,5,2
+Exp,4,6,2
+`;
+
+export const NewYork = `Upgrade,Travel,Hospitality,Attraction
+Bus,0,0,0
+Hostel,1,0,0
+Train,1,0,0
+B&B,1,1,0
+Inn,1,2,0
+New Location,2,3,0
+Dining Experience,2,3,0
+Motel,2,3,1
+Hotel,2,4,1
+Tour Guide,2,3,1
+Plane,2,4,1
+Resort,2,5,1
+Jet,3,5,2
+Exp,4,6,2
+`;
+
+export const Banff = `Upgrade,Travel,Hospitality,Attraction
+Bus,0,0,0
+Hostel,0,0,0
+Train,1,0,0
+B&B,0,1,0
+Inn,0,2,0
+New Location,2,3,0
+Dining Experience,2,3,0
+Motel,2,3,1
+Hotel,2,4,1
+Tour Guide,2,3,1
+Plane,2,3,1
+Resort,2,5,1
+Jet,3,5,2
+Exp,4,6,2
+`;
+
+export const Cabo = `Upgrade,Travel,Hospitality,Attraction
+Bus,0,0,0
+Hostel,0,0,0
+Train,1,0,0
+B&B,0,1,0
+Inn,0,2,0
+Dining Experience,1,1,0
+Motel,0,3,0
+Hotel,0,4,0
+Cruise Ship,2,0,1
+Tour Guide,1,1,1
+New Location,3,5,2
+Plane,3,5,2
+Resort,3,5,2
+Jet,4,5,2
+Exp,5,6,2
+`;
+
+export const Hawaii = `Upgrade,Travel,Hospitality,Attraction
+Plane,0,0,0
+Hostel,1,0,0
+B&B,1,1,0
+Inn,1,2,0
+Dining Experience,1,0,0
+Cruise Ship,1,3,1
+New Location,1,3,1
+Motel,1,3,1
+Hotel,2,4,1
+Tour Guide,1,3,1
+Resort,1,5,1
+Jet,2,5,1
+Exp,3,6,2
+`;
+
+export const BVI = `Upgrade,Travel,Hospitality,Attraction
+Plane,0,0,0
+Hostel,1,0,0
+B&B,1,1,0
+Inn,1,2,0
+Dining Experience,1,0,0
+Cruise Ship,1,3,1
+Motel,1,3,1
+Hotel,2,4,1
+New Location,2,5,1
+Tour Guide,2,5,1
+Resort,2,5,1
+Jet,2,5,1
+Exp,3,6,2
+`;
+
+//TODO ADD MORE
