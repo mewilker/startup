0446cff4e2c60ddcd8c8a4ef847e821deaa2e903
mewilker <mewilk@student.byu.edu>
2024-03-25 23:14:16 -0600

    leaderboard now displays scores

--- startup/public/leaderboard.js
+++ startup/public/leaderboard.js
@@ -1,28 +1,33 @@
-import { getUserCookie } from "./gameplay.js";
-
 let list = document.querySelectorAll("tr");
+
+let rank = 1;
+
 for (let i = 0; i < list.length; i++){
   if (list[i].id != "headers"){
     list[i].remove();
   }
 }
 
-appendRow();
+const res = await fetch('/scores')
+const json = await res.json();
+json.forEach(element => {
+  appendRow(element.user, element.money);
+});
 
-function appendRow(){
+function appendRow(user, money){
     let row = document.createElement("tr")
     let col = document.createElement("td");
     col.setAttribute('class', 'rank');
-    col.textContent = "1";
+    col.textContent = rank++;
     row.appendChild(col);
     col = document.createElement('td');
     col.setAttribute('class', 'user');
-    col.textContent = getUserCookie();
+    col.textContent = user;
     row.appendChild(col);
     col = document.createElement('td');
     col.setAttribute('class', 'score');
     const tycoon = JSON.parse(localStorage.getItem("tycoon"));
-    col.textContent = `$${tycoon.money}`;
+    col.textContent = `$${money}`;
     row.appendChild(col);
 
     document.querySelector('table').appendChild(row);
