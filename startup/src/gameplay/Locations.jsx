1e04fa63896f6c1af3d7117f50fb547c899e5d5f
mewilker <mewilk@student.byu.edu>
2024-05-13 19:02:09 -0600

    leaflet was painful to move to react but I did it yayayay (also it's just the starter map lol)

--- startup/src/gameplay/Locations.jsx
+++ startup/src/gameplay/Locations.jsx
@@ -1,22 +1,30 @@
 import React from "react";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import './gameplay.css';
+import 'leaflet/dist/leaflet.css';
 export function LocationPage(){
     return (
         <main className="agency">
-            <h3>$<span id="money">money goes here</span></h3>
-            <p className="confirm">Select where you want to upgrade!</p>
-            <button className="confirm">Expand!</button>
-            <Map />
+            <h2 id='agencytitle'>Expand your travel agency!</h2>
+            <div className="travelhead">
+                <div className="stats">
+                <h3>$<span id="money">money goes here</span></h3>
+                <p className="confirm">Select where you want to upgrade!</p>
+                <button className="confirm">Expand!</button>
+                </div>
+                <MapContainer style={{flex: 1, height: '100%', margin:0, flexWrap: 'none', 'align-items':'start'}} 
+                    center={[36.0544, -112.1401]} zoom={3} scrollWheelZoom={true}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
         </main>
     )
-}
-
-function Map(){
-    var map = L.map('map').setView([36.0544, -112.1401], 2);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
-    
-    return <div id='map'></div>
 }
