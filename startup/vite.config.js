c3543b43f1e4b1d568b7804ae480f3473c636a5c
mewilker <mewilk@student.byu.edu>
2024-05-15 11:21:56 -0600

    vite config now also goes to 6000

--- startup/vite.config.js
+++ startup/vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:4000',
+      '/api': 'http://localhost:6000',
       '/ws': {
-        target: 'ws://localhost:4000',
+        target: 'ws://localhost:6000',
         ws: true,
       },
     },
